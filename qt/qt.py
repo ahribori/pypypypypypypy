4937cb1ace5a3ed1a2d61bc0963eec51c8a36294
Daniel <ahribori@gmail.com>
2020-09-05 17:04:04 +0900

    PyQt 버튼과 메세지박스

--- qt/qt.py
+++ qt/qt.py
@@ -5,8 +5,18 @@ from PyQt5.QtWidgets import *
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        # 윈도우
         self.setWindowTitle("가즈아!")
         self.setGeometry(300, 300, 300, 400)
+
+        # 버튼
+        button1 = QPushButton("클릭", self)
+        button1.move(20, 20)
+        button1.clicked.connect(self.on_button1_clicked)
+
+    def on_button1_clicked(self):
+        QMessageBox.about(self, "message", "clicked")
 
 
 if __name__ == '__main__':
